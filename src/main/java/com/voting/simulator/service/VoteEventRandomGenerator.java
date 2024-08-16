4984cfc7029981d83287ffe69a038f9a9134383b
Ehsan <ehsan.zangneh@gmail.com>
2024-08-16 16:50:48 +0200

    making thread creation settings configurable

--- src/main/java/com/voting/simulator/service/VoteEventRandomGenerator.java
+++ src/main/java/com/voting/simulator/service/VoteEventRandomGenerator.java
@@ -26,5 +26,4 @@ public class VoteEventRandomGenerator {
          return VoteOptions.values()[ordinal];
     }
 
-
 }
--- src/main/java/com/voting/simulator/service/VoteGeneratorService.java
+++ src/main/java/com/voting/simulator/service/VoteGeneratorService.java
@@ -6,32 +6,49 @@ import com.voting.simulator.common.stream.model.EventMessage;
 import com.voting.simulator.model.VoteEvent;
 import com.voting.simulator.service.dto.GeneratorSwitchDto;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@ConfigurationProperties(prefix = "simulator.thread.settings")
+@Setter
+@Slf4j
 public class VoteGeneratorService {
 
     private final VoteEventRandomGenerator randomGenerator;
     private final StreamPublisher publisher;
+    private int minimumNumberOfThread;
+    private int maximumNumberOfThread;
     private final GeneratorSwitch generatorSwitch = new GeneratorSwitch();
 
     public void start(GeneratorSwitchDto switchDto) {
         this.generatorSwitch.setSleepDuration(switchDto.getDurationSleep());
+        log.info("event generation period is set to {} ms", switchDto.getDurationSleep());
         if(!this.generatorSwitch.isOn()) {
-            var numberOfThreads = switchDto.getNumberOfThreads();
-            numberOfThreads = numberOfThreads < 1 || numberOfThreads > 10 ? 1: numberOfThreads;
-            this.generatorSwitch.setOn(true);
-            for(int i = 1; i <= numberOfThreads; i++) {
-                new Thread(this::triggerGenerator).start();
-            }
+            log.info("Number of threads must be between {} and {}", minimumNumberOfThread, maximumNumberOfThread);
+            setOffTheGenerator(switchDto.getNumberOfThreads());
         }
     }
 
     public void stop() {
-        this.generatorSwitch.setOn(false);
+        if(generatorSwitch.isOn()) {
+            this.generatorSwitch.setOn(false);
+            log.info("generator is switched off");
+        }
+    }
+
+    private void setOffTheGenerator(int numberOfThread) {
+        var numberOfThreads = setNumberOfThreads(numberOfThread);
+        this.generatorSwitch.setOn(true);
+        for(int i = minimumNumberOfThread; i <= numberOfThreads; i++) {
+            new Thread(this::triggerGenerator).start();
+        }
+        log.info("generator is switched on with {} thread(s)", numberOfThreads);
     }
 
     @SneakyThrows
@@ -51,6 +68,11 @@ public class VoteGeneratorService {
                 .topicId(randomGenerator.getTopicId())
                 .voteOptions(randomGenerator.getVote())
                 .build();
+    }
+
+    private int setNumberOfThreads(int suggestedNumberOfThreads) {
+        return suggestedNumberOfThreads < minimumNumberOfThread || suggestedNumberOfThreads > maximumNumberOfThread
+                ? minimumNumberOfThread: suggestedNumberOfThreads;
     }
 
     @Data
